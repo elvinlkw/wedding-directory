bf00748f175617f86e2e907e42ad6b6db070dcc0
Elvin Li <elvinlkw@hotmail.com>
2025-03-30 02:09:11 -0400

    added landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 function App() {
-  return <>Under Construction</>;
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <img
+        style={{ width: '100%', height: '100%', objectFit: 'contain' }}
+        src="/landing.jpeg"
+        alt="Landing Photo"
+      />
+    </div>
+  );
 }
 
 export default App;
